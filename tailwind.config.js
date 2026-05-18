a7d818a0d256728794e4e1a405fa2f9fcb8b8ef4
remyregouby-afk <remy.regouby@hotmail.com>
2026-05-18 20:07:18 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Noto Serif', 'Georgia', 'serif'],
+      }
+    }
+  },
+  plugins: []
+}
